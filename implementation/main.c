92febd439854f7f11038d8570ef66873b2dab1cf
99002568 <71481368+99002568@users.noreply.github.com>
2020-09-19 09:39:34 +0530

    Update main.c

--- implementation/main.c
+++ implementation/main.c
@@ -4,10 +4,20 @@ int main(void)
 {
 
  char name[100];
+ int pin;
  printf("Please Enter Your Name\n");
  gets(name);
  printf("Hello %s, Welcome to our Online Shopping.\n",name);
+ printf("Enter the pin\n");
+ scanf("%d",&pin);
+ if(pin==1111 || pin==2222 || pin==3333 || pin==4444 || pin==5555)
+ {
  shoping();
+ }
+ else{
+    printf("invalid pin\n");
+ }
+
 
  printf("Thanks %s for Choosing Us and Visit us again.\n",name);
 
