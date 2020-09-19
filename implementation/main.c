77eedd2573d8555f25736861453d00a8d9844148
99002568 <71481368+99002568@users.noreply.github.com>
2020-09-19 08:51:33 +0530

    Create main.c

---
+++ implementation/main.c
@@ -0,0 +1,17 @@
+#include "shopping.h"
+
+int main(void)
+{
+
+ char name[100];
+ printf("Please Enter Your Name\n");
+ gets(name);
+ printf("Hello %s, Welcome to our Online Shopping.\n",name);
+ shoping();
+
+ printf("Thanks %s for Choosing Us and Visit us again.\n",name);
+
+ return 0;
+}
+
+
